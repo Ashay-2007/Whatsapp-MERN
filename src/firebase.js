75ef19eee74436dca481127f2c1ab5f4614eff45
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-03-07 21:53:22 +0530

    wireframing the app

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBzL4JVx494OeyyaLnVw6KQZpz1Ot3dJeQ",
+    authDomain: "whatsapp-mern-f05aa.firebaseapp.com",
+    projectId: "whatsapp-mern-f05aa",
+    storageBucket: "whatsapp-mern-f05aa.appspot.com",
+    messagingSenderId: "89049661235",
+    appId: "1:89049661235:web:64d83001e8d4db4dd38846",
+    measurementId: "G-DQHW418YP1"
+  };
