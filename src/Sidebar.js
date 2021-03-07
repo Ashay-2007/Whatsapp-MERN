8e35593bd384b5c94549569a87f4b052c674b434
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-03-07 22:57:48 +0530

    added search container to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -24,6 +24,13 @@ function Sidebar() {
                     </IconButton>
                 </div>
             </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
         </div>
     )
 }
