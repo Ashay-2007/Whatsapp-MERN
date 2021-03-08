de846079fc7375de1479496e42431a66e6d9a01d
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-03-08 14:22:27 +0530

    added chat header

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import './Chat.css';
+import React, { useState } from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from "@material-ui/icons";
+import "./Chat.css";
 
 function Chat() {
     return (
-        <div>
-            
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar />
+                
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
